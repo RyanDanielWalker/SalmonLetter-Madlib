6c7014cba536f2b942d16a3e99b1dea28f54d0a3
Ryan Walker <ryandanielwalker@gmail.com>
2021-03-24 11:09:35 -0700

    Create letter that fades in after name is submitted
    Co-authored-by: Jamie Knutsen <knutsenjamie@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,13 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    const person1Input = $("input#person1").val();
-    const person2Input = $("input#person2").val();
-    const animalInput= $("input#animal").val();
-    const exclamationInput = $("input#exclamation").val();
-    const verbInput = $("input#verb").val();
-    const nounInput = $("input#noun").val();
+  const input1 = $("input#input1").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
+  $(".input1").text(input1);
 
-    
+  $("#letter").fadeIn();
+  $("#formOne").hide();
+  
+  event.preventDefault();
+});
 
-    $("#story").show();
-
-    event.preventDefault();
-  });
 });
